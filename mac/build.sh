4d439359c50a291f02d3158b76b2902b1b95ee53
zlargon <zlargon1988@gmail.com>
2019-08-02 13:17:21 -0400

    [mac] fix macOS version compatibility (target 10.9)
    rebuild fusi static library for target 10.9

--- mac/build.sh
+++ mac/build.sh
@@ -19,11 +19,12 @@ rm -rf bin
 # cmake generate project
 CMAKE_PREFIX_PATH=/usr/local/opt/freetype/:/usr/local/opt/curl/:/usr/local/opt/libogg/:/usr/local/opt/libogg/:/usr/local/opt/libvorbis/:/usr/local/opt/glew/:/usr/local/opt/fribidi/ \
 cmake .. \
-    -DFREETYPE_INCLUDE_DIRS=/usr/local/opt/freetype/include/freetype2/ \
-    -DUSE_SYSTEM_GLEW=1 \
-    -DOPENAL_INCLUDE_DIR=/usr/local/opt/openal-soft/include/ \
-    -DOPENAL_LIBRARY=/usr/local/opt/openal-soft/lib/libopenal.dylib \
-    -DFREETYPE_LIBRARY=/usr/local/opt/freetype/lib/libfreetype.dylib
+    -DFREETYPE_INCLUDE_DIRS=/usr/local/opt/freetype/include/freetype2/  \
+    -DUSE_SYSTEM_GLEW=1                                                 \
+    -DOPENAL_INCLUDE_DIR=/usr/local/opt/openal-soft/include/            \
+    -DOPENAL_LIBRARY=/usr/local/opt/openal-soft/lib/libopenal.dylib     \
+    -DFREETYPE_LIBRARY=/usr/local/opt/freetype/lib/libfreetype.dylib    \
+    -DCMAKE_OSX_DEPLOYMENT_TARGET="10.9"
 
 # cmake build
 make
